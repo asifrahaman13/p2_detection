ad3c08d9fb3e4ab13d6db9bdef43a5ca51705b33
Asif Rahaman <asifrahaman162@gmail.com>
2025-05-18 15:45:33 +0530

    fix: improved the tolerance of the system

--- src/constants/prompt.py
+++ src/constants/prompt.py
@@ -4,21 +4,21 @@ company_name: All company names (noun)\n
 physical_addresses: All physical address like place, country, state etc in the text.\n
 phone_numbers: All phone number in the text\n
 person_names: All the name of the person in the text. (strictly noun)\n
-email_address: All email address in the text\n 
-                     
-give the result in a json format only. 
-                   
+email_address: All email address in the text\n
+
+give the result in a json format only.
+
 The key should be the text and the value should be fom the following generic placeholders.
 
 The person names should not be any list. In fact everything should be key value pairs only. and should contain the following entities only.
 
 - Company name : "THE COMPANY"
 - Company email addresse : "theCOMPANY@email.com"
-- Person name : "THE PERSON" 
+- Person name : "THE PERSON"
 - Addresses : "THE ADDRESS".
-- Phone numbers : +xx yyyyyyyyyy\n  
+- Phone numbers : +xx yyyyyyyyyy\n
 
-             
+
 =======================================================\n\n
 
 *IMPORTANT: Only give the json formatted text and no other response. Your response should be ready to be converted to json.\n
@@ -27,5 +27,5 @@ The person names should not be any list. In fact everything should be key value 
 *IMPORTANT: The results most likelly should not contian any long phrases like "equity investments at fair value through other comprehensive income", "deposits in margin accounts" etc.\n
 
 =======================================================================================>\n
-The given text is: {prompt}.  
+The given text is: {prompt}.
 """
