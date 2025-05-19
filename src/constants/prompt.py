151e4a5ed3df0afaf04a8fe8b428fc6d564628e2
Asif Rahaman <asifrahaman162@gmail.com>
2025-05-19 10:57:07 +0530

    wip: add custom configutations by the users

--- src/constants/prompt.py
+++ src/constants/prompt.py
@@ -15,39 +15,3 @@ The key should strictly be the actual text and the value should be the generic p
 =======================================================================================>\n
 The given text is: {text}.
 """
-
-
-# Example build that should exists:
-
-# prompt_builder = """You are helpful AI assistant. You will be given certain text. Your job is to extract out the following data all in key value pairs. If there are multiple values then they should not be list everything should be key value pairs.
-
-# company_name: All company names (noun)\n
-# email_address: All email address in the text\n
-# physical_addresses: All compnay physical address in the text like "3a fragkokklisias granikou str marousi athens gr 15125 greece".\n
-# person_name: All the name of the person in the text. (strictly noun)\n
-# phone_number: All phone number in the text including +\n
-
-# Give the result in a json format only.\n
-
-# The person names should not be any list. In fact everything should be key value pairs only. and should contain the following entities only. \n
-
-# The key should strictly be the actual text and the value should be the generic placeholders like the following.\n
-
-# - company_name : "THE COMPANY"\n
-# - email_address : "theCOMPANY@email.com"\n
-# - physical_address : "THE ADDRESS".\n
-# - person_name: "THE PERSON"\n
-# - phone_number : +xx yyyyyyyyyy\n
-
-
-# =======================================================\n\n
-
-# *IMPORTANT: Only give the json formatted text and no other response. Your response should be ready to be converted to json.\n
-# *IMPORTANT: Only consider the given entities you are asked for. \n
-# *IMPORTANT: Do not give unnecessary key value pairs other then the format given to you.\n
-# *IMPORTANT: The results most likelly should not contain any long phrases like "equity investments at fair value through other comprehensive income", "deposits in margin accounts", or having more than one entitye like "moldova ms elena matveeva",  "romania ms diana blindu" etc.\n
-# *IMPORTANT: One key value pair should be one entity. For example "bulgaria ms milena boykova" is invalid.
-
-# =======================================================================================>\n
-# The given text is: {prompt}.
-# """
